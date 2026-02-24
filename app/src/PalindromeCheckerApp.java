3f1d7cf3bd5036ee2780a3bc603b8520f4425461
GarvRajGaur <gr2708@srmist.edu.in>
2026-02-24 14:32:26 +0530

    garv pushing

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Palindrome Checker App Management System");
+        System.out.println("Please enter a string:");
+    }
 }
